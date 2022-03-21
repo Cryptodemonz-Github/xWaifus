bb966972b5321ec3e6f6fc6b462d7266fdeb07b2
kanedaaaa <samflynn19121@gmail.com>
2022-03-21 17:43:02 +0100

    erc721 -> erc721a

---
+++ contracts/xWaifusERC721A.sol
@@ -0,0 +1,135 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+/**
+    @title xWaifus NFT contract
+ */
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+import "erc721a/contracts/ERC721A.sol";
+import "erc721a/contracts/extensions/ERC721ABurnable.sol";
+
+contract xWaifusERC721A is ERC721A, ERC721ABurnable, Ownable {
+    using Strings for uint256;
+
+    uint256 TOTAL_SUPPLY = 6666;
+    uint256 PRESALE_SUPPLY = 666;
+
+    uint256 PUBLIC_SALE_PRICE = 0.2 ether;
+    uint256 PRESALE_PRICE = 0.1 ether;
+
+    uint256 PRESALE_MAX_PER_WALLET = 1;
+
+    bool MINTING_ALLOWED = false;
+    bool PUBLIC_MINTING_ALLOWED = false;
+    bool PRESALE_ALLOWED = false;
+
+    string public BEGINNING_URI = "";
+    string public ENDING_URI = "";
+
+    constructor() ERC721A("xWaifus", "xWaifus") {}
+
+    /**
+        @notice mint tokens
+        @param _mode 1 for public minting, 2 for presale minting
+        @param _amount amount of tokens to mint
+     */
+    function mint(uint8 _mode, uint256 _amount) public payable {
+        require(MINTING_ALLOWED, "Minting isn't allowed");
+        if (_mode == 1) {
+            require(
+                PUBLIC_MINTING_ALLOWED,
+                "Either minting or presale isn't allowed"
+            );
+            require(msg.value >= PUBLIC_SALE_PRICE, "Fee isn't paid");
+            require(
+                totalSupply() + _amount <= TOTAL_SUPPLY,
+                "Total supply exceeds max supply"
+            );
+
+            _safeMint(msg.sender, _amount);
+        } else if (_mode == 2) {
+            require(
+                balanceOf(msg.sender) + _amount <= PRESALE_MAX_PER_WALLET,
+                "Too many waifus"
+            );
+            require(
+                PRESALE_ALLOWED,
+                "Either minting or public sale isn't allowed"
+            );
+            require(msg.value >= PRESALE_PRICE, "Fee isn't paid");
+            require(
+                totalSupply() + _amount <= PRESALE_SUPPLY,
+                "Total Supply exceeds max supply"
+            );
+
+            _safeMint(msg.sender, _amount);
+        }
+    }
+
+    /**
+        @notice proper way to burn tokens
+        @param _token_id token id to burn
+     */
+    function burnToken(uint256 _token_id) public {
+        burn(_token_id);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        return
+            string(
+                abi.encodePacked(BEGINNING_URI, tokenId.toString(), ENDING_URI)
+            );
+    }
+
+    /**
+        @notice withdraw ether from contract
+     */
+    function withdraw() public onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    /**
+        @notice set tokenURI
+        @param _mode 1 for BEGINNING_URI and 2 for ENDING_URI
+     */
+    function setURI(uint256 _mode, string memory _new_uri) public onlyOwner {
+        if (_mode == 1) BEGINNING_URI = _new_uri;
+        else if (_mode == 2) ENDING_URI = _new_uri;
+    }
+
+    /**
+        @notice set uint2567 info
+        @param _mode 1 for total supply, 2 for presale supply
+        3 for public sale price, 4 for presale price and
+        5 for max number of tokens per wallet during presale
+     */
+    function setUint256(uint8 _mode, uint256 _value) public onlyOwner {
+        if (_mode == 1) TOTAL_SUPPLY = _value;
+        else if (_mode == 2) PRESALE_SUPPLY = _value;
+        else if (_mode == 3) PUBLIC_SALE_PRICE = _value * (10**18);
+        else if (_mode == 4) PRESALE_PRICE = _value * (10**18);
+        else if (_mode == 5) PRESALE_MAX_PER_WALLET = _value;
+    }
+
+    /**
+        @notice enable minting
+        @param _mode 1 for if minting is allowed,
+        2 for if public minting is allowed, and 3 for
+        presale
+        @param _value allowed or not allowed
+     */
+    function toggleAllowances(uint8 _mode, bool _value) public onlyOwner {
+        if (_mode == 1) MINTING_ALLOWED = _value;
+        else if (_mode == 2) PUBLIC_MINTING_ALLOWED = _value;
+        else if (_mode == 3) PRESALE_ALLOWED = _value;
+    }
+}
---
+++ contracts/xWaifusWhitelist.sol
@@ -0,0 +1,207 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+/**
+    @title xWaifus Whitelist Contract
+    @notice This contract is used to process whitelist sales for xWaifus
+    @dev whitelist is done using merkle root of the list of addresses
+
+    for initial (spawn2) whitelist, user should burn X amount of LLTH to
+    mint 1 xWaifus NFT
+
+    for spawn1 and collab projects whitelists, users should pay X amount of
+    ether to mint max 5 xWaifus NFTs
+ */
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+import "./mocks/xLLTH.sol";
+import "./xWaifusERC721A.sol";
+
+abstract contract xWaifusWhitelist is xWaifusERC721A {
+    uint256 SPAWN_WHITELIST_SUPPLY = 500;
+    uint256 COLLAB_WHITELIST_SUPPLY = 1000;
+
+    uint256 INITIAL_WHITELIST_PRICE = 500 * (10**18);
+    uint256 SPAWN_WHITELIST_PRICE = 0.2 ether;
+    uint256 COLLAB_WHITELIST_PRICE = 0.2 ether;
+
+    uint256 SPAWN_WHITELIST_MAX_PER_WALLET = 5;
+    uint256 COLLAB_WHITELIST_MAX_PER_WALLET = 5;
+
+    bytes32 public INITIAL_WH_MERKLE_ROOT = 0x0;
+    bytes32 public SPAWN_WH_MERKLE_ROOT = 0x0;
+    bytes32 public COLLAB_WH_MERKLE_ROOT = 0x0;
+
+    bool public INITIAL_WH_ENABLED = false;
+    bool public SPAWN_WH_ENABLED = false;
+    bool public COLLAB_WH_ENABLED = false;
+
+    xLLTH public lilith;
+
+    mapping(address => bool) public initialWhClaimed;
+    mapping(address => uint256) public spawnWhClaimed;
+    mapping(address => uint256) public collabWhClaimed;
+
+    constructor(xLLTH _lilith) {
+        lilith = _lilith;
+    }
+
+    /**
+        @notice mint tokens for whitelist
+        @param _mode 0 for spawn2, 1 for spawn1, 2 for collab projects
+        @param _merkleProof merkle proof of the whitelisted addresses
+        @param _amount amount of tokens to mint
+     */
+    function mint(
+        uint8 _mode,
+        bytes32[] calldata _merkleProof,
+        uint256 _amount
+    ) public {
+        if (_mode == 1) {
+            require(
+                INITIAL_WH_ENABLED,
+                "WH not allowed"
+            );
+            require(!initialWhClaimed[msg.sender], "Already claimed");
+            bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
+            require(
+                MerkleProof.verify(_merkleProof, INITIAL_WH_MERKLE_ROOT, leaf),
+                "Invalid merkle proof"
+            );
+
+            lilith.burn(msg.sender, INITIAL_WHITELIST_PRICE);
+            super._safeMint(msg.sender, 1);
+            initialWhClaimed[msg.sender] = true;
+        } else if (_mode == 2) {
+            require(
+                SPAWN_WH_ENABLED,
+                "WH not allowed"
+            );
+            require(
+                spawnWhClaimed[msg.sender] + _amount <=
+                    SPAWN_WHITELIST_MAX_PER_WALLET,
+                "Too many waifus"
+            );
+            bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
+            require(
+                MerkleProof.verify(_merkleProof, SPAWN_WH_MERKLE_ROOT, leaf),
+                "Invalid merkle proof"
+            );
+            super._safeMint(msg.sender, _amount);
+            spawnWhClaimed[msg.sender] += _amount;
+        } else if (_mode == 3) {
+            require(
+                COLLAB_WH_ENABLED,
+                "WH not allowed"
+            );
+            require(
+                collabWhClaimed[msg.sender] + _amount <=
+                    SPAWN_WHITELIST_MAX_PER_WALLET,
+                "Too many waifus"
+            );
+            bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
+            require(
+                MerkleProof.verify(_merkleProof, COLLAB_WH_MERKLE_ROOT, leaf),
+                "Invalid merkle proof"
+            );
+            super._safeMint(msg.sender, _amount);
+            collabWhClaimed[msg.sender] += _amount;
+        }
+    }
+
+    /**
+        @notice set the lilith token 
+        @param _lilith the address of the lilith token
+     */
+    function setLlth(xLLTH _lilith) public onlyOwner {
+        lilith = _lilith;
+    }
+
+    /**
+        @notice set supply settings
+        @param _mode 1 for spawn1, 2 for collab projects.
+        spawn2 whitelist has no hardcoded supply since only certain
+        amount of addresses can mint it and its fixed to 1 per wallet
+     */
+    function setSupply(uint8 _mode, uint256 _amount) public onlyOwner {
+        if (_mode == 1) {
+            SPAWN_WHITELIST_SUPPLY = _amount;
+        } 
+        else if (_mode == 2) {
+            COLLAB_WHITELIST_SUPPLY = _amount;
+        }
+    }
+
+    /**
+        @notice set price settings
+        @param _mode 1 for spawn1, 2 for collab projects.
+        notice that for initial whitelist, the price is denoted in
+        llth and not ether
+     */
+    function setPrices(uint8 _mode, uint256 _amount) public onlyOwner {
+        if (_mode == 1) {
+            INITIAL_WHITELIST_PRICE = _amount*(10**18);
+        } 
+        else if (_mode == 2) {
+            SPAWN_WHITELIST_PRICE = _amount*(10**18);
+        }
+        else if (_mode == 3) {
+            COLLAB_WHITELIST_PRICE = _amount*(10**18);
+        }
+    }
+
+    /**
+        @notice set max per wallet settings
+        @param _mode 1 for spawn1, 2 for collab projects.
+        notice that initial(spawn2) whitelist is fixed to 
+        1 per wallet.
+     */
+    function setMaxPerWallet(uint8 _mode, uint256 _amount) public onlyOwner {
+        if (_mode == 1) {
+            SPAWN_WHITELIST_MAX_PER_WALLET = _amount;
+        } 
+        else if (_mode == 2) {
+            COLLAB_WHITELIST_MAX_PER_WALLET = _amount;
+        }
+    }
+
+    /**
+        @notice set merkle roots
+        @param _initialRoot merkle root of spawn2 whitelist
+        @param _spawnRoot merkle root of spawn1 whitelist
+        @param _collabRoot merkle root of collab projects whitelist
+     */
+    function setMerkleRoots(
+        bytes32 _initialRoot,
+        bytes32 _spawnRoot,
+        bytes32 _collabRoot
+    ) public onlyOwner {
+        INITIAL_WH_MERKLE_ROOT = _initialRoot;
+        SPAWN_WH_MERKLE_ROOT = _spawnRoot;
+        COLLAB_WH_MERKLE_ROOT = _collabRoot;
+    }
+
+    /**
+        @notice enable whitelists
+        @param _mode 1 for spawn2, 2 for spawn1, 3 for collab 
+        and 4 for all
+     */
+    function enable(uint8 _mode, bool _value) public onlyOwner {
+        if (_mode == 1) {
+            INITIAL_WH_ENABLED = _value;
+        } 
+        else if (_mode == 2) {
+            SPAWN_WH_ENABLED = _value;
+        }
+        else if (_mode == 3) {
+            COLLAB_WH_ENABLED = _value;
+        }
+        else if (_mode == 4) {
+            INITIAL_WH_ENABLED = _value;
+            SPAWN_WH_ENABLED = _value;
+            COLLAB_WH_ENABLED = _value;
+        }
+    }
+}
